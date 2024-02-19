090180ce5cc26433037e896887506e2a74557083
Aaron Omale <omalea@student.douglascollege.ca>
2024-02-19 13:10:38 -0800

    chore: initial commit

---
+++ src/main/java/com/csis3275/Group2Assignment2Application.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group2Assignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group2Assignment2Application.class, args);
+	}
+
+}
